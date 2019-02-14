151339ff423f2b6ce50f571fb67eb265be39303d
DELHAY <delhayantoine@gmail.com>
2019-02-14 13:50:52 +0100

    Page version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
 $(function () {
   //navbar
   $('.btnNav').click(function(){
-    var sroll = $(this).attr('data-nav'); ///get  data-nav attribut
-    document.body.scrollTop = scroll; // return to top
-  });
-  // Wrap every letter in a span
-  $('.ml9 .letters').each(function () {
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    var scroll = $(this).attr('data-nav'); ///get  data-nav attribut
+    $(window).scrollTop(scroll);
   });
 
-  anime.timeline({
-      loop: true
-    })
+  // Wrap every letter in a span
+  $('.ml9 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  anime.timeline({loop: true})
     .add({
       targets: '.ml9 .letter',
       scale: [0, 1],
       duration: 4500,
       elasticity: 600,
-      delay: function (el, i) {
-        return 45 * (i + 1)
+      delay: function(el, i) {
+        return 45 * (i+1)
       }
     }).add({
       targets: '.ml9',
@@ -26,5 +24,5 @@ $(function () {
       duration: 1000,
       easing: "easeOutExpo",
       delay: 2000
-    });
+  });
 });
